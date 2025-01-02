cb4356fe7cf468cd3d695eb598e1501492169114
Alina <alinanasirafghan@gmail.com>
2025-01-02 20:23:40 +0500

    recieve budget on frontend

--- node_backend/app.js
+++ node_backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fetch = require("node-fetch"); // Import node-fetch to make HTTP requests
 const predictionRouter = require("./routes/BudgetModelRoute"); // Import the prediction route
+const cors = require("cors"); //middleware for working w angular frontend
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 // /predict is the prediction router
 app.use("/predict", predictionRouter);
