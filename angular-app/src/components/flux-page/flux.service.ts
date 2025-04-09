d5ab1fac8be65d217bfadb3a666dbe198cfeb4e8
Hiba <mallickhiba@gmail.com>
2025-04-09 05:56:03 +0500

    adding ad images to campaign

--- angular-app/src/components/flux-page/flux.service.ts
+++ angular-app/src/components/flux-page/flux.service.ts
@@ -10,10 +10,19 @@ export class FluxService {
 
   constructor(private http: HttpClient) {}
 
+  getAllCampaigns(): Observable<any> {
+    return this.http.get('http://localhost:3000/ads/getAllCampaigns');
+  }
+
+  addImageToCampaign(data: any): Observable<any> {
+    return this.http.post('http://localhost:3000/adImages/add', data);
+  }
+
   generateImage(requestData: any): Observable<any> {
     const headers = new HttpHeaders({
         'Content-Type': 'application/json' // Ensure the request sends JSON data
       });
     console.log('Sending request to Flux API:', requestData);
     return this.http.post<any>(this.apiUrl, requestData, { headers });  }
-}
+
+  }
