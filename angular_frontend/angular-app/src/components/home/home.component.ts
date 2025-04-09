7ac7484eb598555e215718f96289873ce0228990
Alina <alinanasirafghan@gmail.com>
2025-04-09 21:36:54 +0500

    committing changes

--- angular_frontend/angular-app/src/components/home/home.component.ts
+++ angular_frontend/angular-app/src/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+
 })
 export class HomeComponent {
 
