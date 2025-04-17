51260e22e2703b4f208b3158a6baa63ae773f0fb
Alina <alinanasirafghan@gmail.com>
2025-04-17 14:34:25 +0500

    navbar component

--- angular-app/src/components/home/home.component.ts
+++ angular-app/src/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, NavbarComponent],
   templateUrl: './home.component.html',
 
 })
